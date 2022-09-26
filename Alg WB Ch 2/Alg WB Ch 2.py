fbe1be946a0cd5d55284065a724c70b0731e2544
Jongwon Yoon <Jyoon@my.ccri.edu>
2022-09-26 19:39:16 -0400

    update commit

---
+++ Alg WB Ch 2/Alg WB Ch 2.py
@@ -0,0 +1,64 @@
+#Question 1
+
+#Collect users height
+#height = float(input("what is your height? "))
+#print("your height is", height)
+#assign to variable height
+
+
+#Display height
+
+#Question 2
+#Collect user's favorite and assign to variable favorite_color
+#color = input(" what is your favorite color? ")
+#print("your favorite color is", color)
+
+#Question 3
+# a. b = 2 + a
+# b. a = b * 4
+# c. b = a / 3.14
+# d. a = b - 8
+
+#Question 4
+# w = 5
+# x = 4
+# y = 8
+# z = 2
+
+# a. result = x + y
+# b. result = z * 2
+# c. result = y / x
+# d. result = y - z
+# e. result = w // z
+
+#Question 5
+#total = 10 + 14
+
+#Question 6
+#variable_due = variable_total - variaable_down_payment
+
+#Question 7
+#total = subtotal * 0.15
+
+#Question 8
+# a = 5
+# b = 2
+# c = 3
+# result = a + b * c
+# print(result)
+
+#Question 9
+# num = 99
+# num = 5
+# print(num)
+
+#Question 10
+#rounded_value = 0.02
+#print(f"{rounded_value:.2f}")
+
+#Question 11
+#number = 1234567.456
+#print(f"{number:,}")
+
+#Question 12
+#print('George', 'John', 'Paul', 'Ringo', sep='@')
